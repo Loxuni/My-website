235f4474d47c707634c39fbba6e5cecf6823428a
Magnus <magnulow@ntnu.no>
2023-10-21 18:09:14 +0200

    Initialisering av nettsiden

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("klikkMeg");
+    
+    button.addEventListener("click", function() {
+        alert("Du klikket meg!");
+    });
+});
