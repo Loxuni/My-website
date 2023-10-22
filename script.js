c16ab1c031fbaf236ef7e3768729cd2fbab2c5e1
Magnus <magnulow@ntnu.no>
2023-10-22 19:38:38 +0200

    Changes of the structure of the website

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("klikkMeg");
-    
-    button.addEventListener("click", function() {
-        alert("Du klikket meg!");
-    });
-});
